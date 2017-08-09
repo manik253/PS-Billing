b816ae4cc9800a977d2d54a8243812ecc57107dc
manik253 <30829922+manik253@users.noreply.github.com>
2017-08-09 22:30:48 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,58 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+lists = ["soap","shamp","powder"];
+
+
+list = [
+
+{
+	
+	"Name" : "Product1",
+	"Price" : "10000",
+	"Quantity" : "2",
+	}
+	,
+	{
+
+	"Name" : "Product2",
+	"Price" : "177",
+	"Quantity" : "3",
+	
+	}
+
+
+];
+
+
+
+name="";
+price="";
+quantity="";
+pushitem = function()
+{
+	if(this.name != ""  && this.price != "" && this.quantity!="")
+	{
+	this.list.push({'Name':this.name , 'Price':this.price , 'Quantity':this.quantity});
+	
+this.name="";
+this.price="";
+this.quantity="";
+	}
+}
+
+removeitem = function(index)
+{
+	
+	this.lists.splice(index,1);
+}
+
+removeitemarray = function(index)
+{
+	
+	this.list.splice(index,1);
+}
+
+
+
 }
